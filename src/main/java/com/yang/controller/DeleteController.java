b3568a0c2fb4f0454077c031a9ff14abfa339d75
ZhaoYang <yzhao905@outlook.com>
2023-01-12 20:41:16 +0000

    add delete comment function

--- src/main/java/com/yang/controller/DeleteController.java
+++ src/main/java/com/yang/controller/DeleteController.java
@@ -1,5 +1,6 @@
 package com.yang.controller;
 
+import com.yang.mapper.CommentMapper;
 import com.yang.mapper.QuestionMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,18 @@ public class DeleteController {
     @Autowired
     private QuestionMapper questionMapper;
 
+    @Autowired
+    private CommentMapper commentMapper;
+
     @GetMapping("/profile/questions/delete/{id}")
-    public String delete(@PathVariable(name = "id") Long id) {
+    public String deleteQuestion(@PathVariable(name = "id") Long id) {
         questionMapper.deleteByPrimaryKey(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/question/delete/{id}")
+    public String deleteComment(@PathVariable(name = "id") Long id) {
+        commentMapper.deleteByPrimaryKey(id);
         return "redirect:/";
     }
 }
